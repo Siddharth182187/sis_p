510c80ece9d2ec2420c3671594044fb1a2b13aab
Siddharth Gupta <71845547+Siddharth182187@users.noreply.github.com>
2023-06-12 15:01:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import *
+import json, time
 
 app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set = {'Page': 'Home', 'Message': 'Successfully loaded the Home page', 'Timestamp': time.time()}
+    json_dump = json.dumps (data_set)
+    return json_dump
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/user/', methods=['GET'])
+def request_page():
+    user_query= str(request.args.get('user')) # /user/?user=USER_NAME
+    data_set = {'Page': 'Request', 'Message': 'Successfully got the request for' + (user_query), 'Timestamp': time.time()}
+    json_dump = json.dumps (data_set)
+    return json_dump
 
 if __name__ == '__main__':
     app.run()
